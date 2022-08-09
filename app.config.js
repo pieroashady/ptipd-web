0ea8139810cfd9acf07b94b1f0bd9fc3dc6d84b5
pieroashady <aldipiero10@gmail.com>
2022-08-09 21:16:06 +0700

    update app config

--- app.config.js
+++ app.config.js
@@ -1,9 +1,9 @@
 const APP_CONFIG = {
-  baseUrl: "http://localhost:8000/api",
-  ironSessionPassword: "HorsuiKuwXdHYHjYGngYdkSKpyQwtIrC",
-  ironSessionCookieName: "absensi-web",
-  imageDomains: ["localhost"],
-  reservedPath: "/scan-absen",
+  baseUrl: 'http://absensmkn5-kab.xyz//api',
+  ironSessionPassword: 'HorsuiKuwXdHYHjYGngYdkSKpyQwtIrC',
+  ironSessionCookieName: 'absensi-web',
+  imageDomains: ['localhost'],
+  reservedPath: '/scan-absen',
 };
 
 export default APP_CONFIG;
