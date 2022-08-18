d1fa021ae48ce7df880afce9f16ebe745539c0f6
pieroashady <aldipiero10@gmail.com>
2022-08-18 18:56:07 +0700

    change to https

--- app.config.js
+++ app.config.js
@@ -1,5 +1,5 @@
 const APP_CONFIG = {
-  baseUrl: 'http://absensmkn5-kab.xyz/api',
+  baseUrl: 'https://absensmkn5-kab.xyz/api',
   // baseUrl: 'http://localhost:8000/api',
   ironSessionPassword: 'HorsuiKuwXdHYHjYGngYdkSKpyQwtIrC',
   ironSessionCookieName: 'absensi-web',
