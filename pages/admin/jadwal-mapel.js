ab9aa64a678d1b3b05308111f03c6d347d92ddd1
pieroashady <aldipiero11@gmail.com>
2022-08-22 00:20:54 +0700

    fix

--- pages/admin/jadwal-mapel.js
+++ pages/admin/jadwal-mapel.js
@@ -2,10 +2,11 @@ import { Grid } from "@mui/material";
 import { isEmpty } from "ramda";
 import { getJadwalMapel } from "../../lib/service/jadwal-mapel";
 import { getJurusan } from "../../lib/service/jurusan";
+import WithAuth from "../../lib/session/withAuth";
 import JadwalMapelList from "../../src/components/dashboard/dashboard2/JadwalMapelList";
 import JurusanList from "../../src/components/dashboard/dashboard2/JurusanList";
 
-export const getServerSideProps = withAuth(async ({ query }) => {
+export const getServerSideProps = WithAuth(async ({ query }) => {
   const search = isEmpty(query)
     ? ""
     : `${new URLSearchParams(query).toString()}`;
